52d10e633578de9836b6529d2ea12b69cdfa707b
Lucas <lucas_99silva@hotmail.com>
2022-06-03 10:37:21 -0300

    refactor: remove duplicated code

---
+++ src/presentation/test/mock-fake-account.ts
@@ -0,0 +1,8 @@
+import { AccountModel } from '../controllers/signup/signup-protocols'
+
+export const mockFakeAccount = (): AccountModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email@email.com',
+  password: 'valid_password'
+})
