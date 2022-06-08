81aa60e29fdc92a2f1b0f6a94b83b3316dfff812
Lucas <lucas_99silva@hotmail.com>
2022-06-08 11:29:10 -0300

    test: ensure ValidationComposite returns the first if more than one validation fails

--- src/presentation/helpers/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/presentation/helpers/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,4 +1,5 @@
-import { MissingParamError } from '../../../../errors'
+import { InvalidParamError, MissingParamError } from '../../../../errors'
+import { CompareFieldsValidation } from '../compare-fields-validation/compare-fields-validation'
 import { RequiredFieldValidation } from '../required-field-validation/required-field-validation'
 import { ValidationComposite } from './validation-composite'
 
@@ -10,5 +11,15 @@ describe('ValidationComposite', () => {
     const sut = makeSut(validations)
     const error = sut.validate({ other_field: 'any_value' })
     expect(error).toEqual(new MissingParamError('any_field'))
+  })
+
+  test('Should return the first error if more than one validation fails', () => {
+    const validations = [
+      new CompareFieldsValidation('another_field', 'another_field_to_compare'),
+      new RequiredFieldValidation('other_field')
+    ]
+    const sut = makeSut(validations)
+    const error = sut.validate({ other_field: 'any_value', another_field: 'any_another_field', another_field_to_compare: 'invalid_another_field' })
+    expect(error).toEqual(new InvalidParamError('another_field_to_compare'))
   })
 })
