093f6b45248a17870eda02e1ff5745ce5e193cd3
Lucas <lucas_99silva@hotmail.com>
2022-06-08 11:17:32 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/helpers/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/presentation/helpers/validation/validators/validation-composite/validation-composite.spec.ts
@@ -2,10 +2,12 @@ import { MissingParamError } from '../../../../errors'
 import { RequiredFieldValidation } from '../required-field-validation/required-field-validation'
 import { ValidationComposite } from './validation-composite'
 
+const makeSut = (validations = []): ValidationComposite => new ValidationComposite(validations)
+
 describe('ValidationComposite', () => {
   test('Should return an error if any validation fails', () => {
     const validations = [new RequiredFieldValidation('any_field')]
-    const sut = new ValidationComposite(validations)
+    const sut = makeSut(validations)
     const error = sut.validate({ other_field: 'any_value' })
     expect(error).toEqual(new MissingParamError('any_field'))
   })
