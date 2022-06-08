72de97d7039a32de008ed227415564eee7dba67f
Lucas <lucas_99silva@hotmail.com>
2022-06-08 10:57:16 -0300

    test: ensure CompareFieldsValidation not return if comparation succeeds

--- src/presentation/helpers/validation/validators/compare-fields-validation/compare-fields-validation.spec.ts
+++ src/presentation/helpers/validation/validators/compare-fields-validation/compare-fields-validation.spec.ts
@@ -7,4 +7,10 @@ describe('CompareFieldsValidation', () => {
     const error = sut.validate({ any_field: 'any_field', any_field_to_compare: 'invalid_field_to_compare' })
     expect(error).toEqual(new InvalidParamError('any_field_to_compare'))
   })
+
+  test('Should not return if comparation succeeds', () => {
+    const sut = new CompareFieldsValidation('any_field', 'any_field_to_compare')
+    const error = sut.validate({ any_field: 'valid_field', any_field_to_compare: 'valid_field' })
+    expect(error).toBeFalsy()
+  })
 })
