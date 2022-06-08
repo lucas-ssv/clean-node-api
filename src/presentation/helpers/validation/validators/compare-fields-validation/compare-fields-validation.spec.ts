bee712f3ffebe400303a85bea976f5cc71b2de8a
Lucas <lucas_99silva@hotmail.com>
2022-06-08 10:54:55 -0300

    test: ensure CompareFieldsValidation returns an InvalidParamError if comparation fails

---
+++ src/presentation/helpers/validation/validators/compare-fields-validation/compare-fields-validation.spec.ts
@@ -0,0 +1,10 @@
+import { InvalidParamError } from '../../../../errors'
+import { CompareFieldsValidation } from './compare-fields-validation'
+
+describe('CompareFieldsValidation', () => {
+  test('Should return InvalidParamError if comparation fails', () => {
+    const sut = new CompareFieldsValidation('any_field', 'any_field_to_compare')
+    const error = sut.validate({ any_field: 'any_field', any_field_to_compare: 'invalid_field_to_compare' })
+    expect(error).toEqual(new InvalidParamError('any_field_to_compare'))
+  })
+})
