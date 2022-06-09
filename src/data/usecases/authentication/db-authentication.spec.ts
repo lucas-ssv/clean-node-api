d3f666321f9821549b3d09094d38bf20e57b3e1f
Lucas <lucas_99silva@hotmail.com>
2022-06-09 09:33:19 -0300

    refactor: move SUT creation to a factory method

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -1,10 +1,23 @@
 import { LoadAccountByEmailRepositoryStub } from '../../test/mock-load-account-by-email-repository'
 import { DbAuthentication } from './db-authentication'
 
+type SutTypes = {
+  sut: DbAuthentication
+  loadAccountByEmailRepositoryStub: LoadAccountByEmailRepositoryStub
+}
+
+const makeSut = (): SutTypes => {
+  const loadAccountByEmailRepositoryStub = new LoadAccountByEmailRepositoryStub()
+  const sut = new DbAuthentication(loadAccountByEmailRepositoryStub)
+  return {
+    sut,
+    loadAccountByEmailRepositoryStub
+  }
+}
+
 describe('DbAuthentication Usecase', () => {
   test('Should call LoadAccountByEmailRepository with correct email', async () => {
-    const loadAccountByEmailRepositoryStub = new LoadAccountByEmailRepositoryStub()
-    const sut = new DbAuthentication(loadAccountByEmailRepositoryStub)
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
     const loadSpy = jest.spyOn(loadAccountByEmailRepositoryStub, 'load')
     await sut.auth({ email: 'any_email@mail.com', password: 'any_password' })
     expect(loadSpy).toHaveBeenCalledWith('any_email@mail.com')
