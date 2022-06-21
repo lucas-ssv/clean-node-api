9d3e47fba141db0e07c57e83d2a4d2fbfd43bb2a
Lucas <lucas_99silva@hotmail.com>
2022-06-21 11:52:20 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/controllers/survey/add-survey-controller/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey-controller/add-survey-controller.spec.ts
@@ -2,11 +2,24 @@ import { ValidationStub } from '../../../test/mock-validation'
 import { mockAddSurvey } from '../../../test/mock-add-survey'
 import { AddSurveyController } from './add-survey-controller'
 
+type SutTypes = {
+  sut: AddSurveyController
+  validationStub: ValidationStub
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = new ValidationStub()
+  const sut = new AddSurveyController(validationStub)
+  return {
+    sut,
+    validationStub
+  }
+}
+
 describe('AddSurveyController', () => {
   test('Should call Validation with correct values', async () => {
-    const validationStub = new ValidationStub()
+    const { sut, validationStub } = makeSut()
     const validateSpy = jest.spyOn(validationStub, 'validate')
-    const sut = new AddSurveyController(validationStub)
     const httpRequest = mockAddSurvey()
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
