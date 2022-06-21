53b8ef876eb1655728e22196a09b1b142fc14c7a
Lucas <lucas_99silva@hotmail.com>
2022-06-21 14:05:07 -0300

    test: ensure AddSurveyController returns 204 on success

--- src/presentation/controllers/survey/add-survey-controller/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey-controller/add-survey-controller.spec.ts
@@ -1,7 +1,7 @@
 import { ValidationStub } from '../../../test/mock-validation'
 import { AddSurveyStub, mockAddSurveyRequest } from '../../../test/mock-add-survey'
 import { AddSurveyController } from './add-survey-controller'
-import { badRequest, serverError } from '../../../helpers/http/http-helper'
+import { badRequest, noContent, serverError } from '../../../helpers/http/http-helper'
 
 type SutTypes = {
   sut: AddSurveyController
@@ -49,5 +49,11 @@ describe('AddSurveyController', () => {
     jest.spyOn(addSurveyStub, 'add').mockReturnValueOnce(Promise.reject(new Error()))
     const httpResponse = await sut.handle(mockAddSurveyRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('Should return 204 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(mockAddSurveyRequest())
+    expect(httpResponse).toEqual(noContent())
   })
 })
