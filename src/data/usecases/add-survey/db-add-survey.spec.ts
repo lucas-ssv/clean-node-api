ea8b8fa47c6c4f51c82cdfb96fa8ed845c37de26
Lucas <lucas_99silva@hotmail.com>
2022-06-22 09:31:41 -0300

    refactor: move SUT creation to a factory method

--- src/data/usecases/add-survey/db-add-survey.spec.ts
+++ src/data/usecases/add-survey/db-add-survey.spec.ts
@@ -1,11 +1,24 @@
 import { DbAddSurvey } from './db-add-survey'
 import { AddSurveyRepositoryStub, mockAddSurvey } from '../../test/mock-add-survey-repository'
 
+type SutTypes = {
+  sut: DbAddSurvey
+  addSurveyRepositoryStub: AddSurveyRepositoryStub
+}
+
+const makeSut = (): SutTypes => {
+  const addSurveyRepositoryStub = new AddSurveyRepositoryStub()
+  const sut = new DbAddSurvey(addSurveyRepositoryStub)
+  return {
+    sut,
+    addSurveyRepositoryStub
+  }
+}
+
 describe('DbAddSurvey Usecase', () => {
   test('Should call AddSurveyRepository with correct values', async () => {
-    const addSurveyRepositoryStub = new AddSurveyRepositoryStub()
+    const { sut, addSurveyRepositoryStub } = makeSut()
     const addSurveySpy = jest.spyOn(addSurveyRepositoryStub, 'add')
-    const sut = new DbAddSurvey(addSurveyRepositoryStub)
     const surveyData = mockAddSurvey()
     await sut.add(surveyData)
     expect(addSurveySpy).toHaveBeenCalledWith(surveyData)
