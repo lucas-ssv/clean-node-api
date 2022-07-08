df007cb4aec41ad0d5524dd7ba77009da77d3495
Lucas <lucas_99silva@hotmail.com>
2022-07-08 15:08:33 -0300

    feat: ensure DbLoadSurveys calls LoadSurveysRepository

--- src/presentation/controllers/survey/load-surveys-controller/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys-controller/load-surveys-controller.ts
@@ -7,7 +7,7 @@ export class LoadSurveysController implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const surveys = await this.loadSurveys.load()
+      const surveys = await this.loadSurveys.loadAll()
       return surveys.length ? ok(surveys) : noContent()
     } catch (error) {
       return serverError(error)
