1f557cafe79d4753822003b16e2aa7598e49b105
Lucas <lucas_99silva@hotmail.com>
2022-07-08 15:51:22 -0300

    refactor: organize SurveyMongoRepository tests in describes

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -19,19 +19,21 @@ describe('SurveyMongoRepository', () => {
     await MongoHelper.disconnect()
   })
 
-  test('Should return a survey on add success', async () => {
-    const sut = new SurveyMongoRepository()
-    await sut.add({
-      question: 'any_question',
-      answers: [{
-        image: 'any_image',
-        answer: 'any_answer'
-      }, {
-        answer: 'other_answer'
-      }],
-      date: new Date()
+  describe('add()', () => {
+    test('Should return a survey on add success', async () => {
+      const sut = new SurveyMongoRepository()
+      await sut.add({
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }, {
+          answer: 'other_answer'
+        }],
+        date: new Date()
+      })
+      const survey = await surveyCollection.findOne({ question: 'any_question' })
+      expect(survey).toBeTruthy()
     })
-    const survey = await surveyCollection.findOne({ question: 'any_question' })
-    expect(survey).toBeTruthy()
   })
 })
