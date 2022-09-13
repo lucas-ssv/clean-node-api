796f759828a205e226ab49d1a8ad675c8b5e9b02
Lucas <lucas_99silva@hotmail.com>
2022-09-13 09:18:47 -0300

    test: ensure SaveSurveyResultController returns 500 if SurveyById throws

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -2,7 +2,7 @@ import { SaveSurveyResultController } from './save-survey-result-controller'
 import { mockFakeRequest } from '@/presentation/test/mock-fake-request'
 import { LoadSurveyByIdStub } from '@/presentation/test/mock-load-survey-by-id'
 import { LoadSurveyById } from '@/domain/usecases/survey/load-survey-by-id'
-import { forbidden } from '@/presentation/helpers/http/http-helper'
+import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
 import { InvalidParamError } from '@/presentation/errors'
 
 type SutTypes = {
@@ -32,5 +32,14 @@ describe('SaveSurveyResultController', () => {
     jest.spyOn(loadSurveyByIdStub, 'loadById').mockReturnValueOnce(null)
     const httpResponse = await sut.handle(mockFakeRequest())
     expect(httpResponse).toEqual(forbidden(new InvalidParamError('surveyId')))
+  })
+
+  test('Should return 500 if LoadSurveyById throws', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut()
+    jest.spyOn(loadSurveyByIdStub, 'loadById').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const httpResponse = await sut.handle(mockFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
